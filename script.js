16ec0c2102e07011ecc732088488919953926937
Monalins <117097038+Monalins@users.noreply.github.com>
2025-01-30 20:30:00 -0300

    Arquivos R02 do site

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = slides.children.length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    slides.style.transform = `translateX(${-currentSlide * 100}%)`;
+}
+
+setInterval(() => {
+    showSlide(currentSlide + 1);
+}, 3000);
